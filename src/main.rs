9eae854da6e8930a9d6bb56e0133d555321f84b2
aakarshan-raj <aakarshan7raj@gmail.com>
2023-08-02 23:25:53 +0530

    basic echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+
+use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt, BufReader, AsyncBufReadExt}};
+
+#[tokio::main]
+async fn main() {
+
+    let listner = TcpListener::bind("localhost:8030").await.unwrap();
+    loop{
+    let (mut socket,_addr) = listner.accept().await.unwrap();
+   
+    let ( read,mut write) = socket.split();
+
+    let mut buffer = BufReader::new(read);
+    let mut lines = String::new();
+
+    let size = buffer.read_line(&mut lines).await.unwrap();
+    write.write(&mut lines.as_bytes()).await.unwrap();
+
+    print!("Message>{}",lines);
+    }
+
+
+
 }
