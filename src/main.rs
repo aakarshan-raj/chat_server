282571f02044bf029b9b0d086de340fc3a7b80cb
aakarshan-raj <aakarshan7raj@gmail.com>
2023-08-03 23:53:30 +0530

    added broadcast

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
+    net::TcpListener, sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
-    let listner = TcpListener::bind("localhost:8080").await.unwrap();
+    let listner = TcpListener::bind("localhost:8082").await.unwrap();
+    let (tx,rx) = broadcast::channel::<String>(10);
     loop{
+        let txx = tx.clone();
+        let mut rxx = txx.subscribe();
     let (mut socket, _addr) = listner.accept().await.unwrap();
 
     tokio::spawn(async move {
@@ -16,7 +19,10 @@ async fn main() {
             let mut buffer = BufReader::new(read);
             loop {
                 let mut lines = String::new();
-                write.write(&mut lines.as_bytes()).await.unwrap();
+                let size = buffer.read_line(&mut lines).await.unwrap();
+                txx.send(lines.clone()).unwrap();
+                let msg = rxx.recv().await.unwrap();
+                write.write(&mut msg.as_bytes()).await.unwrap();
                 print!("Message>{}", lines);
             }
     });
