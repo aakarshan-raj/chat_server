f4071b54c11c13ef725ea0e010e3f6462baedffd
aakarshan-raj <aakarshan7raj@gmail.com>
2023-08-04 14:19:02 +0530

    added broadbast channels

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener, sync::broadcast,
+    net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listner = TcpListener::bind("localhost:8082").await.unwrap();
-    let (tx,rx) = broadcast::channel::<String>(10);
-    loop{
+    let (tx, rx) = broadcast::channel::<String>(10);
+    loop {
         let txx = tx.clone();
         let mut rxx = txx.subscribe();
-    let (mut socket, _addr) = listner.accept().await.unwrap();
+        let (mut socket, _addr) = listner.accept().await.unwrap();
 
-    tokio::spawn(async move {
-
+        tokio::spawn(async move {
             let (read, mut write) = socket.split();
 
             let mut buffer = BufReader::new(read);
@@ -25,6 +25,6 @@ async fn main() {
                 write.write(&mut msg.as_bytes()).await.unwrap();
                 print!("Message>{}", lines);
             }
-    });
-}
+        });
+    }
 }
